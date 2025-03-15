3fd9b39836f4adcdac8767545cd79c3201166a78
fo <FossatiGiribaldi@gmail.com>
2025-03-15 18:56:56 +0800

    puppeteer

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // index.js
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    headless: true,
+    args: ['--no-sandbox']
+  });
   const page = await browser.newPage();
   await page.goto('https://www.douban.com/');
-  await page.screenshot({ path: 'example.png' });
+
+  // 提取页面标题
+  const title = await page.title();
+
+  // 将标题保存到文件中
+  fs.writeFileSync('crawler-result.txt', title);
+
   await browser.close();
 })();
